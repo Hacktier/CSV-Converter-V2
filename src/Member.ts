eb7ed07251e9b341d891912d4dd355253c634972
Markus Kapp <m.kapp@basecom.de>
2023-10-08 22:16:41 +0200

    state like v1 but in typescript as desktop executable

---
+++ src/Member.ts
@@ -0,0 +1,14 @@
+export type Member = {
+  managerId: number;
+  firstname: string;
+  lastname: string;
+  birthDate: string;
+  gender: number;
+  street: string;
+  zipCode: number;
+  city: string;
+  email: string;
+  phoneNumber: number;
+  phoneNumberExtra: number;
+  membership: number;
+}
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,128 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <input ref="fileInput" type="file" @change="handleFileChange"/>
+    <button @click="convert">Convert</button>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import Papa from 'papaparse'
+import {Member} from "src/Member";
+
+const fileInput = ref<HTMLInputElement>()
+const file = ref<File | null>()
+let members: Member[] = [];
+
+function handleFileChange() {
+  file.value = fileInput.value?.files?.item(0)
+}
+
+async function convert() {
+  if (!file.value) {
+    return
+  }
+
+  readCSV();
+}
+
+
+function writeNewCSV() {
+  /**
+   * * DLRG Manager:        DLRG Seminare
+   *  Frauen = 1          = 1
+   *  Männer = 2          = 0
+   */
+
+  const data = members.map(member => ({
+    'DLRG-Manager-Id': member["managerId"],
+    'Mitgliedsausweisnummer': "",
+    'Vorname': member["firstname"],
+    'Nachname': member["lastname"],
+    'Geburtsdatum': member["birthDate"],
+    'Geschlecht': member["gender"]== 1 ? member["gender"] : 0,
+    'Strasse': member["street"],
+    'PLZ': member["zipCode"],
+    'Wohnort': member["city"],
+    'E-Mail': member["email"],
+    'Telefon privat': member["phoneNumber"],
+    'Telefon mobil': member["phoneNumber"],
+    'Mitgliedschaft (EDVNummer)': member["membership"],
+    'ID UVT': "",
+    'Name Unternehmen': "",
+    'PLZ Unternehmen': "",
+    'Ort Unternehmen': "",
+    'Strasse Unternehmen': "",
+    'Mitgliedsnummer Unternehmen': ""
+  }));
+
+  const newCSV = Papa.unparse(data, {
+    delimiter: ";",
+  });
+
+  download(newCSV)
+}
+
+function download(newCSV: string) {
+  let filename = 'test.csv';
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(newCSV));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+  document.body.removeChild(element);
+
+  file.value = null;
+}
+
+function readCSV() {
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    Papa.parse(reader.result, {
+      header: false,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+      step: function (results: { data: any; errors: any; }, parser: any) {
+
+        if (results.data[0] == "Nummer") {
+          return
+        }
+
+        members.push({
+          managerId: results.data[0],
+          firstname: results.data[2],
+          lastname: results.data[3],
+          birthDate: results.data[9],
+          gender: results.data[1],
+          street: results.data[4],
+          zipCode: results.data[5],
+          city: results.data[6],
+          email: results.data[11],
+          phoneNumber: results.data[7],
+          phoneNumberExtra: results.data[8],
+          membership: 1321012
+        });
+
+        if (results.errors.length !== 0) {
+          console.log("Row errors:", results.errors);
+        }
+      }
+    });
+
+    writeNewCSV();
+
+  };
+
+
+  if (file.value) {
+    reader.readAsText(file.value);
+  }
+}
+
+</script>
+
+
