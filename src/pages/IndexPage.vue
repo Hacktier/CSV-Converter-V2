03e10491311fac0f5ed6f0d8009c2ffc1e8f0d70
Markus Kapp <m.kapp@basecom.de>
2023-10-21 02:36:40 +0200

    first reading of xlsx file -> writing not working

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fileInput position-absolute top-50 start-50 translate-middle row">
-    <input class="form-control col-9" ref="fileInput" type="file" @change="handleFileChange"/>
+    <input class="form-control col-9" ref="fileInput" type="file" @change="handleFileChange" id="input"/>
     <div class="col-3">
       <button class="btn btn-primary  ms-2" @click="convert">Convert</button>
     </div>
@@ -13,6 +13,8 @@ import {ref} from 'vue'
 import Papa from 'papaparse'
 import {Member} from "src/Member";
 import {exportFile, runSequentialPromises} from "quasar";
+import readXlsxFile from "read-excel-file";
+import {Cell} from "read-excel-file/types";
 
 const fileInput = ref<HTMLInputElement>()
 const file = ref<File | null>()
@@ -27,42 +29,52 @@ async function convert() {
     return
   }
 
-  readCSV();
+  readXLSX();
 }
 
 
 async function writeNewCSV() {
+  console.log(members)
+
+  const data = members.map(member => {
+    try {
+      return {
+        'DLRG-Manager-Id': "",
+        // ... (Rest des Codes)
+      };
+    } catch (error) {
+      console.error("Error in mapping:", error)
+    };
 
 
-  const data = members.map(member => ({
-    /**
+      /**
      * * DLRG Manager:        DLRG Seminare
      *  Frauen = 1          = 1
      *  Männer = 2          = 0
      */
 
-    'DLRG-Manager-Id': "",
-    'Mitgliedsausweisnummer': "",
-    'Vorname': member["firstname"],
-    'Nachname': member["lastname"],
-    'Geburtsdatum': member["birthDate"],
-    'Geschlecht': member["gender"] == 1 ? member["gender"] : 0,
-    'Strasse': member["street"],
-    'PLZ': member["zipCode"],
-    'Wohnort': member["city"],
-    'E-Mail': member["email"],
-    'Telefon privat': member["phoneNumber"],
-    'Telefon mobil': member["phoneNumber"],
-    'Mitgliedschaft (EDVNummer)': member["membership"],
-    'ID UVT': "",
-    'Name Unternehmen': "",
-    'PLZ Unternehmen': "",
-    'Ort Unternehmen': "",
-    'Strasse Unternehmen': "",
-    'Mitgliedsnummer Unternehmen': ""
+    // 'DLRG-Manager-Id': "",
+    // 'Mitgliedsausweisnummer': "",
+    // 'Vorname': member["firstname"],
+    // 'Nachname': member["lastname"],
+    // 'Geburtsdatum': member["birthDate"],
+    // 'Geschlecht': member["gender"] === '1' ? member["gender"] : '0',
+    // 'Strasse': member["street"],
+    // 'PLZ': member["zipCode"],
+    // 'Wohnort': member["city"],
+    // 'E-Mail': member["email"],
+    // 'Telefon privat': member["phoneNumber"],
+    // 'Telefon mobil': member["phoneNumber"],
+    // 'Mitgliedschaft (EDVNummer)': '1321012',
+    // 'ID UVT': "",
+    // 'Name Unternehmen': "",
+    // 'PLZ Unternehmen': "",
+    // 'Ort Unternehmen': "",
+    // 'Strasse Unternehmen': "",
+    // 'Mitgliedsnummer Unternehmen': ""
+  });
 
-  }));
-
+  console.log(data)
 
   const fileCount = Math.ceil(data.length / 99);
   const files = [];
@@ -76,7 +88,7 @@ async function writeNewCSV() {
   }
 
   for (let i = 0; i < files.length; i++) {
-   saveFile(files[i])
+    saveFile(files[i])
   }
 
 }
@@ -90,49 +102,39 @@ function saveFile(file: string) {
   );
 }
 
-function readCSV() {
-  const reader = new FileReader();
-
-  reader.onload = () => {
-    Papa.parse(reader.result, {
-      header: false,
-      dynamicTyping: true,
-      skipEmptyLines: true,
-      step: function (results: { data: any; errors: any; }, parser: any) {
-
-        if (results.data[0] == "Nummer") {
-          return
-        }
-
-        members.push({
-          managerId: results.data[0],
-          firstname: results.data[2],
-          lastname: results.data[3],
-          birthDate: results.data[9],
-          gender: results.data[1],
-          street: results.data[4],
-          zipCode: results.data[5],
-          city: results.data[6],
-          email: results.data[11],
-          phoneNumber: results.data[7],
-          phoneNumberExtra: results.data[8],
-          membership: 1321012
-        });
-
-        if (results.errors.length !== 0) {
-          console.log("Row errors:", results.errors);
-        }
-      }
-    });
-
-    writeNewCSV();
-
-  };
-
-
-  if (file.value) {
-    reader.readAsText(file.value);
+function readXLSX() {
+  if (!file.value) {
+    console.log('Nop input');
+    return;
   }
+
+  readXlsxFile(file.value).then(rows => {
+    rows.forEach((row : Cell[]) => {
+      if (row[0] === 'Nummer') {
+        return;
+      }
+
+
+      members.push({
+        managerId: row[0] ? row[0].toString() : '',
+        firstname: row[2] ? row[2].toString() : '',
+        lastname: row[3] ? row[3].toString() : '',
+        birthDate: row[9] ? row[9].toString() : '',
+        gender: row[1] ? row[1].toString() : '',
+        street: row[4] ? row[4].toString() : '',
+        zipCode: row[5] ? row[5].toString() : '',
+        city: row[6] ? row[6].toString() : '',
+        email: row[11] ? row[11].toString() : '',
+        phoneNumber: row[7] ? row[7].toString() : '',
+        phoneNumberExtra: row[8] ? row[8].toString() : '',
+      });
+
+    })
+  })
+
+  writeNewCSV();
+
 }
+
 
 </script>
